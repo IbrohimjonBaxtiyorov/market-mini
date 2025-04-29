270cf543bbf42ac34e64d84f1823dd19f448b28c
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-29 23:31:20 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [data, setdData] = useState([]);
+  const [limit] =useState(18)
+  const [skip ,setSkip]=useState(0)
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/comments?_skip=${skip}&_limit=${limit}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setdData((prev)=>{
+          return [...prev , ...res]
+        });
+        console.log("salonm");
+        
+      });
+  }, [skip]);
+
+  function handleClick(){
+    setSkip((prev)=>{
+       prev+limit
+    })
+  }
+  return (
+    <>
+      <div className="container" id="card-wrapper">
+      {data &&
+        data.map(({ body, name }, index) => {
+          return (
+          
+              <div key={index} className="max-card">
+                <img
+                  src={`https://picsum.photos/320/200?random=${index}`}
+                  alt=""
+                />
+                <div className="card">
+                  <h2 className="title" >{name}</h2>
+                  <p className="text">{body}</p>
+                </div>
+              </div>
+           
+          );
+        })}
+       
+    </div>
+     <div className="container" id="button-card">
+     <button onClick={handleClick} className="add-button">Ko'proq ma'lumotlar</button>
+
+     </div>
+    </>
+  
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header-container">
+        <div  className="container">
+        <h1 className="header-title">Api Malumotlar</h1>
+        </div>
+       
+    </header>
+  )
+}
+
+export default Header
